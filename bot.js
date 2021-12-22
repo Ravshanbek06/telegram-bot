928da6b9753a8c1444475e67cf983b4682e0abea
Codership <sharipboyevr@gmail.com>
2021-12-22 16:00:40 +0500

    1st bot

---
+++ bot.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+console.log(process.env.TOKEN);
+
+const TelegramBot = require('node-telegram-bot-api')
+
+const bot = new TelegramBot(process.env.TOKEN, { polling: true })
+bot.on("polling_error", console.log)
+
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, "Salom, " + msg.chat.first_name)
+})
